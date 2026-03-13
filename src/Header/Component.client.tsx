684868ebea73c3c6e584a7af4b3fce3723fced4f
Eli Lemons <me@elilemons.com>
2026-03-13 18:09:42 -0400

    Quick fixes for the mobile view

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -1,12 +1,12 @@
 'use client'
-import { useHeaderTheme } from '@/providers/HeaderTheme'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
+import type React from 'react'
+import { useEffect, useState } from 'react'
+import { Logo } from '@/components/Logo/Logo'
 
 import type { Header } from '@/payload-types'
-
-import { Logo } from '@/components/Logo/Logo'
+import { useHeaderTheme } from '@/providers/HeaderTheme'
 import { HeaderNav } from './Nav'
 
 interface HeaderClientProps {
@@ -19,21 +19,30 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
   const { headerTheme, setHeaderTheme } = useHeaderTheme()
   const pathname = usePathname()
 
+  // biome-ignore lint/correctness/useExhaustiveDependencies: from Payload
   useEffect(() => {
     setHeaderTheme(null)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pathname])
 
+  // biome-ignore lint/correctness/useExhaustiveDependencies: from Payload
   useEffect(() => {
     if (headerTheme && headerTheme !== theme) setTheme(headerTheme)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [headerTheme])
 
   return (
-    <header className="container relative z-20   " {...(theme ? { 'data-theme': theme } : {})}>
-      <div className="py-8 flex justify-between">
+    <header
+      className="container relative z-20   "
+      {...(theme ? { 'data-theme': theme } : {})}
+    >
+      <div className="py-8 flex md:flex-row flex-col items-center justify-between">
         <Link href="/">
-          <Logo loading="eager" priority="high" className="invert dark:invert-0" />
+          <Logo
+            loading="eager"
+            priority="high"
+            className="invert dark:invert-0"
+          />
         </Link>
         <HeaderNav data={data} />
       </div>
